cb428d99cb1a0c7901430a595c8bd328ff579ee8
LevP21 <21levperminov@gmail.com>
2025-09-18 14:51:12 +0700

    Add function

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
+import os
+
 import nltk
 from nltk import word_tokenize, ngrams
 from nltk.probability import FreqDist
 
-with open("input.txt", "r") as f:
-    text = f.read()
+def find_ngrams(input_file):
+    if not os.path.exists(input_file):
+        print(f"Входной файл {input_file} не найден")
+        return
+
+    with open(input_file, "r") as f:
+        text = f.read()
+
+    tokens = word_tokenize(text.lower())
+
+    bigrams = list(ngrams(tokens, 2))
+    trigrams = list(ngrams(tokens, 3))
+
+    freq_bigrams = FreqDist(bigrams)
+    freq_trigrams = FreqDist(trigrams)
+
+    print("Топ-5 биграмм:")
+    for bigram, count in freq_bigrams.most_common(5):
+        print(f"{' '.join(bigram)}: {count}")
+
+    print("\nТоп-5 триграмм:")
+    for trigram, count in freq_trigrams.most_common(5):
+        print(f"{' '.join(trigram)}: {count}")
+
 
 nltk.download('punkt_tab')
 
-tokens = word_tokenize(text.lower())
+input_path = "input.txt"
 
-bigrams = list(ngrams(tokens, 2))
-trigrams = list(ngrams(tokens, 3))
-
-freq_bigrams = FreqDist(bigrams)
-freq_trigrams = FreqDist(trigrams)
-
-print("Топ-5 биграмм:")
-for bigram, count in freq_bigrams.most_common(5):
-    print(f"{' '.join(bigram)}: {count}")
-
-print("\nТоп-5 триграмм:")
-for trigram, count in freq_trigrams.most_common(5):
-    print(f"{' '.join(trigram)}: {count}")
+find_ngrams(input_path)
